eec333f643be3786830e35c053a87f910e4b4dc6
Dapp Dev <dappdev9@gmail.com>
2020-04-26 13:41:01 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Tailwindcss from './Tailwindcss.svelte';
+
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<Tailwindcss />
+
+<div class="p-2 border bg-white rounded-lg shadow-2xl">
+	<h1>Hello {name}!</h1>
+</div>
